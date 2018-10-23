91e45c833dfd19d49ccf28b10d3d24b00f294e2e
Sagar Devkota <sagarda7@yahoo.com>
2018-10-23 20:35:56 +0545

    transfer api changes

--- routes/apis/v1/trips.js
+++ routes/apis/v1/trips.js
@@ -5,6 +5,7 @@ const jwt = require('jsonwebtoken');
 const config = require('../../../config/database');
 var Trip = require('../../../models/trip');
 var Bus = require('../../../models/bus');
+var Passenger = require('../../../models/passenger');
 var Notification = require('../../../models/notification');
 
 //Authenticate (Gives back a token)
@@ -60,7 +61,7 @@ router.put('/end/:id', function (req, res) {
     });
 });
 
-router.put('/transfer/:id', function (req, res) {
+router.put('/transferold/:id', function (req, res) {
     return Trip.findById(req.params.id, function (errr, trip) {
         var b_num = req.body.bus_num;
         if(!errr) {
@@ -100,6 +101,73 @@ router.put('/transfer/:id', function (req, res) {
         }
        
     });
+});
+
+
+router.put('/transfer/:id', function (req, res) {
+    var b_num = req.body.bus_num;
+    return Bus.findOne({
+        bus_num: b_num
+    }, function (err, doc) {
+        if (doc) {
+            var bus_id = doc._id;
+
+            return Trip.findOne({bus_id:bus_id, trip_completed: false}, function (errr, trip) {
+                if(trip) {
+                    var tripId = trip._id;
+                    Passenger.update({trip_id:req.params.id}, {trip_id: tripId}, {multi: true}, 
+                        function(err, num) {
+                            if(!err) {
+                                return Trip.findOne({_id:req.params.id}, function (errrr, trp) {
+                                    if(trp) {
+                                        trp.bus_id = bus_id;
+                                        return trp.save(function (errrrr) {
+                                            if (!errrrr) {
+                                                return res.json({
+                                                    success: true,
+                                                    trip_info: trp
+                                                });
+                                                // console.log("updated"+" "+req.body.Depart_time+" "+req.body.Depart_location);
+                                            } else {
+                                                return res.json({
+                                                    success: false,
+                                                    message: "Could not update bus info in current trip",
+                                                    details: err
+                                                });
+                                            }
+                                        });
+                                    } else {
+
+                                    }
+                                });
+                            } else {
+                                return res.json({
+                                    success: false,
+                                    message: "Could not transfer passenger",
+                                    details: err
+                                });
+                            }
+                        }
+                    );
+        
+                } else {
+                    return res.json({
+                        success: false,
+                        message: "Running Trip not found for given bus number"
+                    });
+                }
+               
+            });
+            
+        } else {
+            return res.json({
+                success: false,
+                message: "Bus not found"
+            });
+        }
+    });
+
+    
 });
 
 
